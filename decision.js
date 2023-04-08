544756a66dddb68d7b7a4ad5a2a0a9a4d59b0a58
hoibest <130141508+hcha3232@users.noreply.github.com>
2023-04-08 14:16:44 +1000

    update

--- decision.js
+++ decision.js
@@ -85,7 +85,7 @@ const questionList = {
         <br>\
         Differential diagnosis: 
         ${buttonModal('CMV AU','Cytomegalovirus Anterior Uveitis',null,'cmv')}
-        ${buttonModal('HSV AU','Herpes Simplex Virus Anterior Uveitis',null,'hsv')}
+        ${buttonModal('HSV AU','Herpes Simplex Virus Anterior Uveitis',disDatabase('hsv'),'hsv')}
         ${buttonModal('VZV AU','Varicella Zoster Virus Anterior Uveitis',null,'vzv')}`,
         options: [
         ]
@@ -271,3 +271,49 @@ function buttonModal(btnName,headerName,disName_database,disId){
     return btnModalHTML;
 }
 
+function disDatabase(dis){
+    database = {
+        hsv : `
+        <div class="container" id="main-article">
+            <div>SUN Classification Criteria</div>
+            <div class="main-table">
+                <div style="font-weight: 400">Should satisfy #1 AND #2 AND #3</div>
+                <br>
+                <ol class="no-padding">
+                    <li>Evidence of anterior uveitis
+                        <ol type="a" style="font-weight:400;" class="no-padding">
+                            <li>Anterior chamber cells</li>
+                            <li>If anterior vitreous cells are present, severity is less than anterior chamber inflammation</li>
+                            <li>No evidence of retinitis</li>
+                        </ol>
+                    </li>
+                    <br>
+                    <li>Unilateral uveitis (unless there is a positive aqueous PCR for herpes simplex virus)
+                    </li>
+                    <br>
+                    <li>Evidence of herpes simplex infection in the eye
+                        <ol type="a" style="font-weight:400;" class="no-padding">
+                            <li>Aqueous humor PCR positive for herpes simplex virus OR</li>
+                            <li>Sectoral iris atrophy in a patient ≤50 years of age OR</li>
+                            <li>Herpes simplex keratitis</li>
+                        </ol>
+                    </li>
+                </ol>
+                <br>
+                <div class="exclusion-box" id="exclusion-box">
+                    <div class="exclusion-title">
+                        <div style="font-weight:700">Exclude if any criterion below is fulfilled</div>
+                    </div>
+                    <br>
+                    <ul style="font-weight:400;" class="no-padding">
+                        <li>Concomitant dermatomal/cutaneous varicella zoster virus (unless aqueous specimen PCR positive for herpes simplex virus)</li>
+                        <li>Positive serology for syphilis using a treponemal test</li>
+                        <li>Evidence of sarcoidosis (either bilateral hilar adenopathy on chest imaging or tissue biopsy demonstrating non-caseating granulomata)</li>
+                        <li>Aqeous specimen PCR positive for cytomegalovirus or varicella zoster virus</li>
+                    </ul>
+                </div>
+            </div>
+        </div>`
+    }
+    return database[dis]
+}
